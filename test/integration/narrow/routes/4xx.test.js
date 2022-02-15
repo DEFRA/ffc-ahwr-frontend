14ae5fea74e2a0d50b1b42d75fb17cfb3d836186
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-02-15 16:24:44 +0000

    test: extend test to cover phase banner

--- test/integration/narrow/routes/4xx.test.js
+++ test/integration/narrow/routes/4xx.test.js
@@ -21,6 +21,9 @@ describe('4xx error pages', () => {
     const $ = cheerio.load(res.payload)
     expect($('.govuk-heading-l').text()).toEqual('404 - Not Found')
     expect($('.govuk-body').text()).toEqual('Not Found')
+    expect($('.govuk-phase-banner').length).toEqual(1)
+    expect($('.govuk-phase-banner').text()).toMatch('beta')
+    expect($('.govuk-phase-banner').text()).toMatch('This is a new service - your feedback will help us to improve it.')
   })
 
   afterEach(async () => {
