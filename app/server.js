1220647f1e537563717b6a63a4d760214d4a382f
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-02-22 15:42:16 +0000

    feat: add generic logger. log route table

--- app/server.js
+++ app/server.js
@@ -33,8 +33,13 @@ async function createServer () {
   await server.register(require('@hapi/inert'))
   await server.register(require('./plugins/auth'))
   await server.register(require('./plugins/error-pages'))
+  await server.register(require('./plugins/logging'))
   await server.register(require('./plugins/router'))
   await server.register(require('./plugins/views'))
+
+  if (config.isDev) {
+    await server.register(require('blipp'))
+  }
 
   return server
 }
