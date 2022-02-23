e108bb3b2b2e15ba174e89725fe907287f478fd3
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-02-23 09:56:45 +0000

    feat: add confirmation route

--- app/routes/farmer-apply/confirmation.js
+++ app/routes/farmer-apply/confirmation.js
@@ -1,29 +1,17 @@
+const { getEligibleOrgs } = require('../../api-requests/orgs')
 const { v4: uuidv4 } = require('uuid')
-const boom = require('@hapi/boom')
-const { notify: { templateIdApplicationComplete } } = require('../../config')
-const { cacheKeys } = require('../../config/constants')
-const sendEmail = require('../../lib/send-email')
 
-// TODO: Where should a GET request to the route go?
 module.exports = {
-  method: 'POST',
+  method: 'GET',
   path: '/farmer-apply/confirmation',
   options: {
     handler: async (request, h) => {
       // TODO: Get this data based on eligibility or the applicant
-      // const { sbi } = request.payload
-      const organisation = request.yar.get(cacheKeys.org)
-
+      const { sbi } = request.query
+      const organisation = getEligibleOrgs().find(x => x.sbi === sbi)
       // TODO: should the reference number be a particular format?
       const reference = uuidv4().split('-').shift().toLocaleUpperCase('en-GB')
-
-      // TODO: Check an email hasn't been sent already and store the fact that this has been sent
-      const result = await sendEmail(templateIdApplicationComplete, organisation.email, { personalisation: { name: organisation.name, reference }, reference })
-
-      if (!result) {
-        return boom.internal()
-      }
-
+      // TODO: send email via Notify
       return h.view('farmer-apply/confirmation', { reference })
     }
   }
