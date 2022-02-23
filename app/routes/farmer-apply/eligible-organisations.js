f8e8daa0879304284cf1b0ef6fb5ccd157e310ef
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-02-23 08:47:57 +0000

    refactor: use govukTable component

--- app/routes/farmer-apply/eligible-organisations.js
+++ app/routes/farmer-apply/eligible-organisations.js
@@ -7,7 +7,14 @@ module.exports = {
     handler: async (_, h) => {
       // TODO: Get this data based on eligibility
       const organisations = getEligibleOrgs()
-      return h.view('farmer-apply/eligible-organisations', { organisations })
+      const rows = organisations.map(org => {
+        return [{ text: org.name }, { text: org.sbi }, { text: 0 }, { html: `<a href="/farmer-apply/org-review?sbi=${org.sbi}">View or start application</a>` }]
+      })
+      const tableData = {
+        head: [{ text: 'Organisation' }, { text: 'SBI Number' }, { text: 'Applications' }, { text: '' }],
+        rows
+      }
+      return h.view('farmer-apply/eligible-organisations', { orgsToShow: organisations.length > 0, tableData })
     }
   }
 }
