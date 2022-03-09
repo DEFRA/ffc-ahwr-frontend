18117d4bf9f550cb4688c575710fb6991c8d4211
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2022-03-09 11:34:15 +0000

    Move away from function expressions in module

--- app/session/index.js
+++ app/session/index.js
@@ -3,17 +3,35 @@ const entries = {
   organisation: 'organisation'
 }
 
-const set = (request, entryKey, key, value) => {
+function set (request, entryKey, key, value) {
   const entryValue = request.yar?.get(entryKey) || {}
   entryValue[key] = value
   request.yar.set(entryKey, entryValue)
 }
 
-const get = (request, entryKey, key) => key ? request.yar?.get(entryKey)?.[key] : request.yar?.get(entryKey)
+function get (request, entryKey, key) {
+  return key ? request.yar?.get(entryKey)?.[key] : request.yar?.get(entryKey)
+}
+
+function setApplication (request, key, value) {
+  set(request, entries.application, key, value)
+}
+
+function setOrganisation (request, key, value) {
+  set(request, entries.organisation, key, value)
+}
+
+function getApplication (request, key) {
+  return get(request, entries.application, key)
+}
+
+function getOrganisation (request, key) {
+  return get(request, entries.organisation, key)
+}
 
 module.exports = {
-  setApplication: (request, key, value) => set(request, entries.application, key, value),
-  setOrganisation: (request, key, value) => set(request, entries.organisation, key, value),
-  getApplication: (request, key) => get(request, entries.application, key),
-  getOrganisation: (request, key) => get(request, entries.organisation, key)
+  setApplication,
+  setOrganisation,
+  getApplication,
+  getOrganisation
 }
