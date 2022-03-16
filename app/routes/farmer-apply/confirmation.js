1a8d007b5ff6c0a56bfa057512b914c99e5064bb
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-03-16 14:22:24 +0000

    refactor: rename function for simplicity

--- app/routes/farmer-apply/confirmation.js
+++ app/routes/farmer-apply/confirmation.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuid } = require('uuid')
 const boom = require('@hapi/boom')
 const { notify: { templateIdApplicationComplete } } = require('../../config')
 const sendEmail = require('../../lib/send-email')
@@ -15,7 +15,7 @@ module.exports = {
       const organisation = session.getOrganisation(request)
 
       // TODO: should the reference number be a particular format?
-      const reference = uuidv4().split('-').shift().toLocaleUpperCase('en-GB')
+      const reference = uuid().split('-').shift().toLocaleUpperCase('en-GB')
 
       // TODO: Check an email hasn't been sent already and store the fact that this has been sent
       const result = await sendEmail(templateIdApplicationComplete, organisation.email, { personalisation: { name: organisation.name, reference }, reference })
