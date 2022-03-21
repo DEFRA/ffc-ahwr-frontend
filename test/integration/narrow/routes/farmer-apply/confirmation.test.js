0db87a8e529f22bafc114a85af1a29b2dca83378
Shivanshu Patel <shivanshuit914@gmail.com>
2022-03-21 11:50:51 +0000

    fix: broken tests

--- test/integration/narrow/routes/farmer-apply/confirmation.test.js
+++ test/integration/narrow/routes/farmer-apply/confirmation.test.js
@@ -1,13 +1,19 @@
+const getCrumbs = require('../../../../utils/get-crumbs')
+
 const auth = { credentials: { reference: '1111', sbi: '111111111' }, strategy: 'basic' }
 
 describe('Confirmation test', () => {
-  test('GET /farmer-apply/declaration route returns 200', async () => {
+  test('POST /farmer-apply/confirmation route returns 200', async () => {
+    const crumb = await getCrumbs(global.__SERVER__)
     const options = {
-      method: 'GET',
+      method: 'POST',
       url: '/farmer-apply/confirmation',
-      auth
+      payload: { crumb },
+      auth,
+      headers: { cookie: `crumb=${crumb}` }
     }
     const res = await global.__SERVER__.inject(options)
     expect(res.statusCode).toBe(200)
+    expect(res.payload).toContain('Application complete')
   })
 })
--- test/integration/narrow/routes/farmer-apply/declaration.test.js
+++ test/integration/narrow/routes/farmer-apply/declaration.test.js
@@ -1,5 +1,3 @@
-const getCrumbs = require('../../../../utils/get-crumbs')
-
 const auth = { credentials: { reference: '1111', sbi: '111111111' }, strategy: 'basic' }
 
 describe('Declaration test', () => {
@@ -11,18 +9,6 @@ describe('Declaration test', () => {
     }
     const res = await global.__SERVER__.inject(options)
     expect(res.statusCode).toBe(200)
-  })
-  test('POST /farmer-apply/declaration route returns 302', async () => {
-    const crumb = await getCrumbs(global.__SERVER__)
-    const options = {
-      method: 'POST',
-      url: '/farmer-apply/declaration',
-      payload: { crumb },
-      auth,
-      headers: { cookie: `crumb=${crumb}` }
-    }
-    const res = await global.__SERVER__.inject(options)
-    expect(res.statusCode).toBe(302)
-    expect(res.headers.location).toEqual('/farmer-apply/confirmation')
+    expect(res.payload).toContain('Declaration')
   })
 })
