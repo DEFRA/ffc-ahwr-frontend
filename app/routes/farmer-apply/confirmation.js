658ff461ade493a6eb8fd75fb81eda910fa25db3
Satish Chatap <satish.chatap@capgemini.com>
2022-03-22 11:23:59 +0000

    added session id in message

--- app/routes/farmer-apply/confirmation.js
+++ app/routes/farmer-apply/confirmation.js
@@ -1,9 +1,10 @@
 const { v4: uuidv4 } = require('uuid')
 const boom = require('@hapi/boom')
-const { notify: { templateIdApplicationComplete }, applicationRequestQueue, applicationRequestMsgType } = require('../../config')
+const { notify: { templateIdApplicationComplete }, applicationRequestQueue, applicationRequestMsgType, applicationResponseQueue } = require('../../config')
 const sendEmail = require('../../lib/send-email')
 const session = require('../../session')
-const sendMessage = require('../../messaging/send-message')
+const { sendMessage, receiveMessage } = require('../../messaging')
+const util = require('util')
 
 // TODO: Where should a GET request to the route go?
 module.exports = {
@@ -17,10 +18,15 @@ module.exports = {
       // TODO: should the reference number be a particular format?
       const reference = uuidv4().split('-').shift().toLocaleUpperCase('en-GB')
       session.setApplication(request, 'applicationId', reference)
+      session.setApplication(request, 'sessionId', request.yar.id)
 
       const application = session.getApplication(request)
       sendMessage(application, applicationRequestMsgType, applicationRequestQueue, { sessionId: request.yar.id })
+      const response = await receiveMessage(request.yar.id, applicationResponseQueue)
 
+      if (response) {
+        console.info('Response received:', util.inspect(response, false, null, true))
+      }
       // TODO: Check an email hasn't been sent already and store the fact that this has been sent
       const result = await sendEmail(templateIdApplicationComplete, organisation.email, { personalisation: { name: organisation.name, reference }, reference })
 
