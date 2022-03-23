1dd841d750b1380029e39248f04883c8c9588608
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-03-23 16:39:55 +0000

    feat: add tests for user data request

---
+++ test/unit/app/api-requests/users.test.js
@@ -0,0 +1,54 @@
+const { storage: { connectionString, usersContainer, usersFile } } = require('../../../../app/config')
+
+describe('Get users', () => {
+  let downloadBlobMock
+  let getByEmail
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    jest.resetModules()
+
+    downloadBlobMock = require('../../../../app/lib/download-blob')
+    jest.mock('../../../../app/lib/download-blob')
+
+    const users = require('../../../../app/api-requests/users')
+    getByEmail = users.getByEmail
+  })
+
+  test('makes request to download users blob', async () => {
+    await getByEmail('email')
+
+    expect(downloadBlobMock).toHaveBeenCalledTimes(1)
+    expect(downloadBlobMock).toHaveBeenCalledWith(connectionString, usersContainer, usersFile)
+  })
+
+  test.each([
+    { fileContent: null },
+    { fileContent: undefined }
+  ])('return undefined when blob content is null or undefined', async ({ fileContent }) => {
+    downloadBlobMock.mockResolvedValue(fileContent)
+
+    const res = await getByEmail('email')
+
+    expect(res).toEqual(undefined)
+  })
+
+  test('return undefined when email doesn\'t match any users', async () => {
+    const fileContent = '[{ "email": "a@b.com" }]'
+    downloadBlobMock.mockResolvedValue(fileContent)
+
+    const res = await getByEmail('miss@email.com')
+
+    expect(res).toEqual(undefined)
+  })
+
+  test('return user data when email is matched', async () => {
+    const email = 'hit@email.com'
+    const fileContent = `[{ "email": "${email}" }]`
+    downloadBlobMock.mockResolvedValue(fileContent)
+
+    const res = await getByEmail(email)
+
+    expect(res).toEqual(JSON.parse(fileContent)[0])
+  })
+})
