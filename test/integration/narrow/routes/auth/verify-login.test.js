f154ad37ec0a07e269c6d37ae4ea8c6f4d4cecd6
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-03-25 13:19:59 +0000

    refactor: include test case info in test title

--- test/integration/narrow/routes/auth/verify-login.test.js
+++ test/integration/narrow/routes/auth/verify-login.test.js
@@ -23,7 +23,7 @@ describe('Verify login page test', () => {
     { email: '' },
     { email: 'not-an-email' },
     { email: 'email@not-on-list.com' }
-  ])('GET /verify-login route returns 400 when request does not include a valid email', async ({ email }) => {
+  ])('GET /verify-login route returns 400 when request does not include a valid email - $email', async ({ email }) => {
     const options = {
       method: 'GET',
       url: `${url}?email=${email}&token=${validToken}`
@@ -41,7 +41,7 @@ describe('Verify login page test', () => {
     { token: '' },
     { token: 'not-a-uuid' },
     { token: validToken }
-  ])('GET /verify-login route returns 400 when request does not include a valid token', async ({ token }) => {
+  ])('GET /verify-login route returns 400 when request does not include a valid token - $token', async ({ token }) => {
     const options = {
       method: 'GET',
       url: `${url}?email=${validEmail}&token=${token}`
