716994f55c055e43dc366f3be99259813bd37f68
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-03-28 09:14:24 +0100

    feat: increase test coverage for check-answers route

--- test/integration/narrow/routes/farmer-apply/check-answers.test.js
+++ test/integration/narrow/routes/farmer-apply/check-answers.test.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio')
 
 const varListTemplate = {
   cattle: 'yes',
-  pig: 'yes',
+  pigs: 'yes',
   sheep: 'yes',
   cattleType: 'both'
 }
@@ -33,14 +33,16 @@ describe('Check Answers test', () => {
       url,
       auth
     }
+
     const res = await global.__SERVER__.inject(options)
+
     expect(res.statusCode).toBe(200)
   })
 
-  test(`GET ${url} route returns 302`, async () => {
+  test(`GET ${url} route returns 302 when there is no eligible livestock`, async () => {
     varList = {
       cattle: 'no',
-      pig: 'no',
+      pigs: 'no',
       sheep: 'no',
       cattleType: 'both'
     }
@@ -49,27 +51,62 @@ describe('Check Answers test', () => {
       url,
       auth
     }
+
     const res = await global.__SERVER__.inject(options)
+
     expect(res.statusCode).toBe(302)
     expect(res.headers.location).toEqual('/farmer-apply/not-eligible')
   })
 
-  test(`GET ${url} route returns 302`, async () => {
+  test.each([
+    { cattleType: 'beef', text: 'Beef' },
+    { cattleType: 'both', text: 'Beef and Dairy' },
+    { cattleType: 'dairy', text: 'Dairy' }
+  ])(`GET ${url} route shows beef and dairy option when cattleType is both - %s`, async ({ cattleType, text }) => {
     varList = {
       cattle: 'yes',
-      pig: 'no',
+      pigs: 'no',
       sheep: 'no',
-      cattleType: 'both'
+      cattleType
     }
     const options = {
       method: 'GET',
       url,
       auth
     }
+
     const res = await global.__SERVER__.inject(options)
+
     expect(res.statusCode).toBe(200)
     const $ = cheerio.load(res.payload)
-    expect($('.govuk-summary-list').text()).toContain('Beef and Dairy')
+    expect($('.govuk-summary-list__row').length).toEqual(2)
+    expect($('.govuk-summary-list__key').eq(0).text()).toMatch('Livestock')
+    expect($('.govuk-summary-list__value').eq(0).text()).toMatch('More than 10 cattle')
+    expect($('.govuk-summary-list__key').eq(1).text()).toMatch('Cattle type')
+    expect($('.govuk-summary-list__value').eq(1).text()).toMatch(text)
+  })
+
+  test(`GET ${url} route does not show beef and dairy option when cattle is not selected is both`, async () => {
+    varList = {
+      cattle: 'no',
+      pigs: 'yes',
+      sheep: 'yes',
+      cattleType: ''
+    }
+    const options = {
+      method: 'GET',
+      url,
+      auth
+    }
+
+    const res = await global.__SERVER__.inject(options)
+
+    expect(res.statusCode).toBe(200)
+    const $ = cheerio.load(res.payload)
+    expect($('.govuk-summary-list__row').length).toEqual(1)
+    expect($('.govuk-summary-list__key').eq(0).text()).toMatch('Livestock')
+    expect($('.govuk-summary-list__value').eq(0).text()).toMatch('More than 50 pigs')
+    expect($('.govuk-summary-list__value').eq(0).text()).toMatch('More than 20 sheep')
   })
 
   test(`GET ${url} route when not logged in redirects to /login with last page as next param`, async () => {
