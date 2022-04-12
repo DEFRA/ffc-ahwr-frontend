5792f14a33dbebd7590695c2e3dfbef244a51893
Shivanshu Patel <shivanshuit914@gmail.com>
2022-04-12 16:33:54 +0100

    feat: add page for privacy policy

---
+++ app/routes/privacy-policy.js
@@ -0,0 +1,10 @@
+module.exports = {
+  method: 'GET',
+  path: '/privacy-policy',
+  options: {
+    auth: false,
+    handler: async (_, h) => {
+      return h.view('privacy-policy', { title: 'Privacy Policy' })
+    }
+  }
+}
