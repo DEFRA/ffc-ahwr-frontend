aa03739c7d2d03c0d982ce03f586633e69626471
Shivanshu Patel <shivanshuit914@gmail.com>
2022-04-13 15:09:59 +0100

    Privacy policy page (#31)
    * feat: add page for privacy policy
    
    * feat: add privacy policy route and menu item
    
    * feat: add integration test for privacy policy page
    
    * feat: bump package version

---
+++ test/integration/narrow/routes/privacy-policy.test.js
@@ -0,0 +1,17 @@
+const cheerio = require('cheerio')
+const expectPhaseBanner = require('../../../utils/phase-banner-expect')
+const { serviceName } = require('../../../../app/config')
+
+describe('Privacy Policy', () => {
+  test('should load page successfully', async () => {
+    const options = {
+      method: 'GET',
+      url: '/privacy-policy'
+    }
+    const response = await global.__SERVER__.inject(options)
+    expect(response.statusCode).toBe(200)
+    const $ = cheerio.load(response.payload)
+    expect($('h1.govuk-heading-l').text()).toEqual(`Privacy Policy statement for ${serviceName} service`)
+    expectPhaseBanner.ok($)
+  })
+})
