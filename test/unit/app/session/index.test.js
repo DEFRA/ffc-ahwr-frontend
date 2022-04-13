73e2328cd90a1fb6a6890b6f7dbfaea7254af7f7
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-04-13 14:39:13 +0100

    refactor: reduce duplication and ease adding new session tests

--- test/unit/app/session/index.test.js
+++ test/unit/app/session/index.test.js
@@ -1,113 +1,85 @@
 const session = require('../../../../app/session')
 
 describe('session', () => {
-  const getApplication = 'getApplication'
-  const getOrganisation = 'getOrganisation'
-  const getVetSignup = 'getVetSignup'
-  const setApplication = 'setApplication'
-  const setOrganisation = 'setOrganisation'
-  const setVetSignup = 'setVetSignup'
   const applicationSectionKey = 'application'
   const organisationSectionKey = 'organisation'
   const vetSignupSectionKey = 'vetSignup'
+  const vetVisitSectionKey = 'vetVisitData'
 
   const value = 'value'
   const objectValue = { key: value }
 
-  test.each([
-    { func: getApplication, expectedSectionKey: applicationSectionKey, key: 'key', value },
-    { func: getApplication, expectedSectionKey: applicationSectionKey, key: 'unknown', value: undefined },
-    { func: getApplication, expectedSectionKey: applicationSectionKey, key: false, value: objectValue },
-    { func: getApplication, expectedSectionKey: applicationSectionKey, key: null, value: objectValue },
-    { func: getApplication, expectedSectionKey: applicationSectionKey, key: undefined, value: objectValue },
-    { func: getOrganisation, expectedSectionKey: organisationSectionKey, key: 'key', value },
-    { func: getOrganisation, expectedSectionKey: organisationSectionKey, key: 'unknown', value: undefined },
-    { func: getOrganisation, expectedSectionKey: organisationSectionKey, key: false, value: objectValue },
-    { func: getOrganisation, expectedSectionKey: organisationSectionKey, key: null, value: objectValue },
-    { func: getOrganisation, expectedSectionKey: organisationSectionKey, key: undefined, value: objectValue },
-    { func: getVetSignup, expectedSectionKey: vetSignupSectionKey, key: 'key', value },
-    { func: getVetSignup, expectedSectionKey: vetSignupSectionKey, key: 'unknown', value: undefined },
-    { func: getVetSignup, expectedSectionKey: vetSignupSectionKey, key: false, value: objectValue },
-    { func: getVetSignup, expectedSectionKey: vetSignupSectionKey, key: null, value: objectValue },
-    { func: getVetSignup, expectedSectionKey: vetSignupSectionKey, key: undefined, value: objectValue }
-  ])('$func retrieves value from $expectedSectionKey based on key (key value - $key)', async ({ func, expectedSectionKey, key, value }) => {
-    let sectionKey
-    const requestGetMock = { yar: { get: (entryKey) => { sectionKey = entryKey; return objectValue } } }
+  const getFunctionsToTest = [
+    { func: 'getApplication', expectedSectionKey: applicationSectionKey },
+    { func: 'getOrganisation', expectedSectionKey: organisationSectionKey },
+    { func: 'getVetSignup', expectedSectionKey: vetSignupSectionKey },
+    { func: 'getVetVisitData', expectedSectionKey: vetVisitSectionKey }
+  ]
 
-    const application = session[func](requestGetMock, key)
+  const setFunctionsToTest = [
+    { func: 'setApplication', expectedSectionKey: applicationSectionKey },
+    { func: 'setOrganisation', expectedSectionKey: organisationSectionKey },
+    { func: 'setVetSignup', expectedSectionKey: vetSignupSectionKey },
+    { func: 'setVetVisitData', expectedSectionKey: vetVisitSectionKey }
+  ]
 
-    expect(application).toEqual(value)
-    expect(sectionKey).toEqual(expectedSectionKey)
+  const keysAndValuesToTest = [
+    { key: 'key', value },
+    { key: 'unknown', value: undefined },
+    { key: false, value: objectValue },
+    { key: null, value: objectValue },
+    { key: undefined, value: objectValue }
+  ]
+
+  describe.each(getFunctionsToTest)('"$func" retrieves value from "$expectedSectionKey" based on key value', ({ func, expectedSectionKey }) => {
+    test.each(keysAndValuesToTest)('key value - $key', async ({ key, value }) => {
+      let sectionKey
+      const requestGetMock = { yar: { get: (entryKey) => { sectionKey = entryKey; return objectValue } } }
+
+      const application = session[func](requestGetMock, key)
+
+      expect(application).toEqual(value)
+      expect(sectionKey).toEqual(expectedSectionKey)
+    })
   })
 
-  test.each([
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: 'key', value },
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: 'unknown', value: undefined },
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: false, value: objectValue },
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: null, value: objectValue },
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: undefined, value: objectValue },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: 'key', value },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: 'unknown', value: undefined },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: false, value: objectValue },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: null, value: objectValue },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: undefined, value: objectValue },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: 'key', value },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: 'unknown', value: undefined },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: false, value: objectValue },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: null, value: objectValue },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: undefined, value: objectValue }
-  ])('$func sets value in $expectedSectionKey based on the key (key value - $key) when no value exists in $expectedSectionKey', async ({ func, expectedSectionKey, key, value }) => {
-    const yarMock = {
-      get: jest.fn(),
-      set: jest.fn()
-    }
-    const requestSetMock = { yar: yarMock }
+  describe.each(setFunctionsToTest)('"$func" sets value in "$expectedSectionKey" based on key value when no value exists in "$expectedSectionKey"', ({ func, expectedSectionKey }) => {
+    test.each(keysAndValuesToTest)('key value - $key', async ({ key, value }) => {
+      const yarMock = {
+        get: jest.fn(),
+        set: jest.fn()
+      }
+      const requestSetMock = { yar: yarMock }
 
-    session[func](requestSetMock, key, value)
+      session[func](requestSetMock, key, value)
 
-    expect(requestSetMock.yar.get).toHaveBeenCalledTimes(1)
-    expect(requestSetMock.yar.get).toHaveBeenCalledWith(expectedSectionKey)
-    expect(requestSetMock.yar.set).toHaveBeenCalledTimes(1)
-    expect(requestSetMock.yar.set).toHaveBeenCalledWith(expectedSectionKey, { [key]: value })
+      expect(requestSetMock.yar.get).toHaveBeenCalledTimes(1)
+      expect(requestSetMock.yar.get).toHaveBeenCalledWith(expectedSectionKey)
+      expect(requestSetMock.yar.set).toHaveBeenCalledTimes(1)
+      expect(requestSetMock.yar.set).toHaveBeenCalledWith(expectedSectionKey, { [key]: value })
+    })
   })
 
-  test.each([
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: 'key', value },
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: 'unknown', value: undefined },
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: false, value: objectValue },
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: null, value: objectValue },
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: undefined, value: objectValue },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: 'key', value },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: 'unknown', value: undefined },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: false, value: objectValue },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: null, value: objectValue },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: undefined, value: objectValue },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: 'key', value },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: 'unknown', value: undefined },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: false, value: objectValue },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: null, value: objectValue },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: undefined, value: objectValue }
-  ])('$func sets value in $expectedSectionKey based on the key (key value - $key) when a value already exists in $expectedSectionKey', async ({ func, expectedSectionKey, key, value }) => {
-    const existingValue = { existingKey: 'existing-value' }
-    const yarMock = {
-      get: jest.fn(() => existingValue),
-      set: jest.fn()
-    }
-    const requestSetMock = { yar: yarMock }
+  describe.each(setFunctionsToTest)('"$func" sets value in "$expectedSectionKey" based on key when a value already exists in "$expectedSectionKey"', ({ func, expectedSectionKey }) => {
+    test.each(keysAndValuesToTest)('key value - $key', async ({ key, value }) => {
+      const existingValue = { existingKey: 'existing-value' }
+      const yarMock = {
+        get: jest.fn(() => existingValue),
+        set: jest.fn()
+      }
+      const requestSetMock = { yar: yarMock }
 
-    session[func](requestSetMock, key, value)
+      session[func](requestSetMock, key, value)
 
-    expect(requestSetMock.yar.get).toHaveBeenCalledTimes(1)
-    expect(requestSetMock.yar.get).toHaveBeenCalledWith(expectedSectionKey)
-    expect(requestSetMock.yar.set).toHaveBeenCalledTimes(1)
-    expect(requestSetMock.yar.set).toHaveBeenCalledWith(expectedSectionKey, { ...{ [key]: value }, ...existingValue })
+      expect(requestSetMock.yar.get).toHaveBeenCalledTimes(1)
+      expect(requestSetMock.yar.get).toHaveBeenCalledWith(expectedSectionKey)
+      expect(requestSetMock.yar.set).toHaveBeenCalledTimes(1)
+      expect(requestSetMock.yar.set).toHaveBeenCalledWith(expectedSectionKey, { ...{ [key]: value }, ...existingValue })
+    })
   })
 
-  test.each([
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: 'key', value: '    to be trimmed   ' },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: 'key', value: '    to be trimmed   ' },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: 'key', value: '    to be trimmed   ' }
-  ])('$func sets value once trimmed when the value is a string (value = "$value")', async ({ func, expectedSectionKey, value }) => {
+  const valueToBeTrimmed = '    to be trimmed   '
+  test.each(setFunctionsToTest)(`"$func" sets value once trimmed, when the value is a string (value = "${valueToBeTrimmed}")`, async ({ func, expectedSectionKey }) => {
     const key = 'key'
     const yarMock = {
       get: jest.fn(),
@@ -115,19 +87,15 @@ describe('session', () => {
     }
     const requestSetMock = { yar: yarMock }
 
-    session[func](requestSetMock, key, value)
+    session[func](requestSetMock, key, valueToBeTrimmed)
 
     expect(requestSetMock.yar.get).toHaveBeenCalledTimes(1)
     expect(requestSetMock.yar.get).toHaveBeenCalledWith(expectedSectionKey)
     expect(requestSetMock.yar.set).toHaveBeenCalledTimes(1)
-    expect(requestSetMock.yar.set).toHaveBeenCalledWith(expectedSectionKey, { [key]: value.trim() })
+    expect(requestSetMock.yar.set).toHaveBeenCalledWith(expectedSectionKey, { [key]: valueToBeTrimmed.trim() })
   })
 
-  test.each([
-    { func: setApplication, expectedSectionKey: applicationSectionKey, key: 'key', value: objectValue },
-    { func: setOrganisation, expectedSectionKey: organisationSectionKey, key: 'key', value: objectValue },
-    { func: setVetSignup, expectedSectionKey: vetSignupSectionKey, key: 'key', value: objectValue }
-  ])('$func does not trim value when the value is not a string (value = "$value")', async ({ func, expectedSectionKey, value }) => {
+  test.each(setFunctionsToTest)(`"$func" does not trim value when the value is not a string (value = "${objectValue}")`, async ({ func, expectedSectionKey }) => {
     const key = 'key'
     const yarMock = {
       get: jest.fn(),
@@ -135,11 +103,11 @@ describe('session', () => {
     }
     const requestSetMock = { yar: yarMock }
 
-    session[func](requestSetMock, key, value)
+    session[func](requestSetMock, key, objectValue)
 
     expect(requestSetMock.yar.get).toHaveBeenCalledTimes(1)
     expect(requestSetMock.yar.get).toHaveBeenCalledWith(expectedSectionKey)
     expect(requestSetMock.yar.set).toHaveBeenCalledTimes(1)
-    expect(requestSetMock.yar.set).toHaveBeenCalledWith(expectedSectionKey, { [key]: value })
+    expect(requestSetMock.yar.set).toHaveBeenCalledWith(expectedSectionKey, { [key]: objectValue })
   })
 })
