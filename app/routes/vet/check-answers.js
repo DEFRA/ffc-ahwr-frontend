b6f46ed428b289e05f6409aca1d8c76bb064a229
Shivanshu Patel <shivanshuit914@gmail.com>
2022-04-14 14:05:36 +0100

    feat: add integratin test for vet check answers page

--- app/routes/vet/check-answers.js
+++ app/routes/vet/check-answers.js
@@ -3,8 +3,8 @@ const { vetVisitData } = require('../../session/keys')
 
 const backLink = '/vet/visit-date'
 
-function getVisitDate (application) {
-  const visitDate = application[vetVisitData.visitDate]
+function getVisitDate (vetVisit) {
+  const visitDate = vetVisit[vetVisitData.visitDate]
   return visitDate.map(item => item.value).join(' ')
 }
 
@@ -13,10 +13,10 @@ module.exports = {
   path: '/vet/check-answers',
   options: {
     handler: async (request, h) => {
-      const application = session.getVetVisitData(request)
+      const vetVisit = session.getVetVisitData(request)
       const rows = [{
         key: { text: 'Farm visit date' },
-        value: { html: getVisitDate(application) },
+        value: { html: getVisitDate(vetVisit) },
         actions: { items: [{ href: '/vet/visit-date', text: 'Change', visuallyHiddenText: 'name' }] }
       }]
 
---
+++ test/integration/narrow/routes/vet/check-answers.test.js
@@ -0,0 +1,65 @@
+const cheerio = require('cheerio')
+const expectPhaseBanner = require('../../../../utils/phase-banner-expect')
+const session = require('../../../../../app/session')
+
+session.getVetVisitData = jest.fn().mockReturnValue({
+  visitDate: [
+    { name: 'day', classes: 'govuk-input--width-2', value: 12 },
+    { name: 'month', classes: 'govuk-input--width-2', value: 4 },
+    { name: 'year', classes: 'govuk-input--width-4', value: 2022 }
+  ]
+})
+
+describe('Vet check answers test', () => {
+  afterAll(() => {
+    jest.resetAllMocks()
+  })
+
+  const auth = { credentials: {}, strategy: 'cookie' }
+  const url = '/vet/check-answers'
+
+  describe(`GET ${url} route`, () => {
+    test('returns 200', async () => {
+      const options = {
+        method: 'GET',
+        url,
+        auth
+      }
+      const res = await global.__SERVER__.inject(options)
+      expect(res.statusCode).toBe(200)
+      const $ = cheerio.load(res.payload)
+      expect($('h1.govuk-heading-l').text()).toEqual('Check your answers')
+      expect($('title').text()).toEqual('Check your answers')
+      expectPhaseBanner.ok($)
+    })
+
+    test('when not logged in redirects to /vet', async () => {
+      const options = {
+        method: 'GET',
+        url
+      }
+
+      const res = await global.__SERVER__.inject(options)
+
+      expect(res.statusCode).toBe(302)
+      expect(res.headers.location).toEqual('/vet')
+    })
+
+    test('does not show beef and dairy option when cattle is not selected is both', async () => {
+      const options = {
+        method: 'GET',
+        url,
+        auth
+      }
+
+      const res = await global.__SERVER__.inject(options)
+
+      expect(res.statusCode).toBe(200)
+      const $ = cheerio.load(res.payload)
+      expect($('.govuk-summary-list__row').length).toEqual(1)
+      expect($('.govuk-summary-list__key').eq(0).text()).toMatch('Farm visit date')
+      expect($('.govuk-summary-list__value').eq(0).text()).toMatch('12 4 2022')
+      expect($('.govuk-summary-list__actions .govuk-link').eq(0).text()).toMatch('Change')
+    })
+  })
+})
