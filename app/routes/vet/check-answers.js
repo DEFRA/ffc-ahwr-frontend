c6f178f1233c70222c5e777441a9fd63fbf86e73
Shivanshu Patel <shivanshuit914@gmail.com>
2022-04-14 12:59:19 +0100

    feat: implement view for vet check answers page

--- app/routes/vet/check-answers.js
+++ app/routes/vet/check-answers.js
@@ -1,15 +1,24 @@
 const session = require('../../session')
+const { vetVisitData } = require('../../session/keys')
 
 const backLink = '/vet/visit-date'
+
+function getVisitDate(application) {
+  const visitDate = application[vetVisitData.visitDate];
+  return visitDate.map(item => item.value).join(' ');
+}
 
 module.exports = {
   method: 'GET',
   path: '/vet/check-answers',
   options: {
     handler: async (request, h) => {
-      const application = session.getApplication(request)
-
-      const rows = []
+      const application = session.getVetVisitData(request)
+      const rows = [{
+        key: { text: 'Farm visit date' },
+        value: { html: getVisitDate(application) },
+        actions: { items: [{ href: '/vet/visit-date', text: 'Change', visuallyHiddenText: 'name' }] }
+      }]
 
       return h.view('vet/check-answers', { listData: { rows }, backLink })
     }
