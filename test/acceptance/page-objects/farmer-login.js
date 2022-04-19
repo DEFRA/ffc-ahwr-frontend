90845125596c11d93093478d3f332d17ed4ae887
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-04-19 13:54:43 +0100

    feat: add accetpance tests

---
+++ test/acceptance/page-objects/farmer-login.js
@@ -0,0 +1,21 @@
+import Page from './page'
+
+class FarmerLogin extends Page {
+  get email () { return $('#email') }
+  get signin () { return $('#submit') }
+
+  open () {
+    super.open('')
+    browser.pause(3000)
+  }
+
+  async enterEmail (email) {
+    await (await this.email).setValue(email)
+  }
+
+  async clickSignin () {
+    await (await this.signin).click()
+  }
+}
+
+export default new FarmerLogin()
---
+++ test/acceptance/wdio.conf.js
@@ -0,0 +1,107 @@
+const { ReportAggregator, HtmlReporter } = require('@rpii/wdio-html-reporter')
+const log4js = require('@log4js-node/log4js-api')
+const path = require('path')
+const logger = log4js.getLogger('default')
+const envRoot = (process.env.TEST_ENVIRONMENT_ROOT_URL || 'http://host.docker.internal:3000')
+const chromeArgs = process.env.CHROME_ARGS ? process.env.CHROME_ARGS.split(' ') : []
+const maxInstances = process.env.MAX_INSTANCES ? Number(process.env.MAX_INSTANCES) : 1
+
+exports.config = {
+  outputDir: path.resolve(__dirname, './logs'),
+  chromeArgs,
+  specs: ['./features/**/*.feature'],
+  exclude: ['./scratch/**'],
+  maxInstances,
+
+  // ===================
+  // Test Configurations
+  // ===================
+  // Define all options that are relevant for the WebdriverIO instance here
+  logLevel: 'warn',
+  bail: 0,
+  baseUrl: envRoot,
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 1,
+  framework: 'cucumber',
+  specFileRetries: 0,
+  specFileRetriesDelay: 30,
+  reporters: ['spec',
+    [HtmlReporter, {
+      debug: false,
+      outputDir: './html-reports/',
+      filename: 'feature-report.html',
+      reportTitle: 'Feature Test Report',
+      showInBrowser: false,
+      useOnAfterCommandForScreenshot: false,
+      LOG: logger
+    }]
+  ],
+  // If you are using Cucumber you need to specify the location of your step definitions.
+  cucumberOpts: {
+    require: ['./steps/**/*.js'], // <string[]> (file/dir) require files before executing features
+    backtrace: false, // <boolean> show full backtrace for errors
+    requireModule: ['@babel/register'], // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+    dryRun: false, // <boolean> invoke formatters without executing steps
+    failFast: false, // <boolean> abort the run on first failure
+    format: ['pretty'], // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+    colors: true, // <boolean> disable colors in formatter output
+    snippets: true, // <boolean> hide step definition snippets for pending steps
+    source: true, // <boolean> hide source uris
+    profile: [], // <string[]> (name) specify the profile to use
+    strict: false, // <boolean> fail if there are any undefined or pending steps
+    tagExpression: '', // <string> (expression) only execute the features or scenarios with tags matching the expression
+    timeout: 60000, // <number> timeout for step definitions
+    ignoreUndefinedDefinitions: false // <boolean> Enable this config to treat undefined definitions as warnings.
+  },
+  // ====
+  // Hooks
+  // =====
+  onPrepare: async function (config, capabilities) {
+    const reportAggregator = new ReportAggregator({
+      outputDir: './html-reports/',
+      filename: 'acceptance-test-suite-report.html',
+      reportTitle: 'Acceptance Tests Report',
+      browserName: capabilities.browserName
+    })
+    reportAggregator.clean()
+    global.reportAggregator = reportAggregator
+  },
+
+  onComplete: function (exitCode, config, capabilities, results) {
+    (async () => {
+      await global.reportAggregator.createReport()
+    })()
+  },
+
+  before: function () {
+    const chai = require('chai')
+    global.expect = chai.expect
+    global.assert = chai.assert
+    global.should = chai.should()
+  },
+
+  afterStep: async (step, scenario, result, context) => {
+    if (result.passed) {
+      return
+    }
+
+    const path = require('path')
+    const moment = require('moment')
+    const screenshotFileName = scenario.uri.split('.feature')[0].split('/').slice(-1)[0]
+    const timestamp = moment().format('YYYYMMDD-HHmmss.SSS')
+    const filepath = path.join('./html-reports/screenshots/', screenshotFileName + '-' + timestamp + '.png')
+    await browser.saveScreenshot(filepath)
+    process.emit('test:screenshot', filepath)
+  },
+
+  /**
+   *
+   * Runs before a Cucumber Scenario.
+   * @param {ITestCaseHookParameter} world    world object containing information on pickle and test step
+   * @param {Object}                 context  Cucumber World object
+   */
+  beforeScenario: async (world, context) => {
+    await browser.deleteCookie('ffc_ahwr_auth')
+  }
+}
