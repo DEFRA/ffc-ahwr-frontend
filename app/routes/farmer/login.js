f67d6f9b7fcbbd19be27dc19fd9b1b4e9479bde2
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-04-27 09:06:49 +0100

    refactor: claim and apply login to single route

--- app/routes/farmer/login.js
+++ app/routes/farmer/login.js
@@ -1,16 +1,34 @@
 const boom = require('@hapi/boom')
 const Joi = require('joi')
 const { getByEmail } = require('../../api-requests/users')
-const { sendFarmerApplyLoginMagicLink } = require('../../lib/email/send-magic-link-email')
+const { sendFarmerApplyLoginMagicLink, sendFarmerClaimLoginMagicLink } = require('../../lib/email/send-magic-link-email')
 const { email: emailValidation } = require('../../../app/lib/validation/email')
 
-// TODO: refactor this to share with farmer-claim/login
+const validJourneys = {
+  apply: 'apply',
+  claim: 'claim'
+}
+
+function getLoggedInPath (journey) {
+  switch (journey) {
+    case validJourneys.apply:
+      return '/farmer-apply/org-review'
+    case validJourneys.claim:
+      return '/farmer-claim/visit-review'
+  }
+}
+
 module.exports = [{
   method: 'GET',
-  path: '/farmer-apply/login',
+  path: '/farmer-{journey}/login',
   options: {
     auth: {
       mode: 'try'
+    },
+    validate: {
+      params: Joi.object({
+        journey: Joi.string().valid(...Object.values(validJourneys))
+      })
     },
     plugins: {
       'hapi-auth-cookie': {
@@ -18,15 +36,18 @@ module.exports = [{
       }
     },
     handler: async (request, h) => {
+      console.log(request.params.journey)
+      const { journey } = request.params
       if (request.auth.isAuthenticated) {
-        return h.redirect(request.query?.next || '/farmer-apply/org-review')
+        const loggedInPath = getLoggedInPath(journey)
+        return h.redirect(request.query?.next || loggedInPath)
       }
       return h.view('auth/magic-login')
     }
   }
 }, {
   method: 'POST',
-  path: '/farmer-apply/login',
+  path: '/farmer-{journey}/login',
   options: {
     auth: {
       mode: 'try'
@@ -34,6 +55,9 @@ module.exports = [{
     validate: {
       payload: Joi.object({
         email: emailValidation
+      }),
+      params: Joi.object({
+        journey: Joi.string().valid(...Object.values(validJourneys))
       }),
       failAction: async (request, h, error) => {
         return h.view('auth/magic-login', { ...request.payload, errorMessage: { text: error.details[0].message } }).code(400).takeover()
@@ -47,7 +71,12 @@ module.exports = [{
         return h.view('auth/magic-login', { ...request.payload, errorMessage: { text: `No user found with email address "${email}"` } }).code(400).takeover()
       }
 
-      const result = await sendFarmerApplyLoginMagicLink(request, email)
+      let result
+      if (request.params.journey === validJourneys.apply) {
+        result = await sendFarmerApplyLoginMagicLink(request, email)
+      } else if (request.params.journey === validJourneys.claim) {
+        result = await sendFarmerClaimLoginMagicLink(request, email)
+      }
 
       if (!result) {
         return boom.internal()
