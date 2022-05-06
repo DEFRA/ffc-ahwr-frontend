1a9300fab5f63be3f3743c7dab88e9e0c4df0196
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-05-06 14:40:52 +0100

    feat: add yes/no radios for visit review

--- app/routes/farmer-claim/visit-review.js
+++ app/routes/farmer-claim/visit-review.js
@@ -1,8 +1,14 @@
 const boom = require('@hapi/boom')
+const getYesNoRadios = require('../helpers/yes-no-radios')
 const { getClaim } = require('../../session')
 const { getClaimAmount } = require('../../lib/get-claim-amount')
 
-module.exports = {
+const legendText = 'Are these details correct?'
+const radioId = 'details-correct'
+// const errorText = 'Select yes if these details are correct'
+// const backLink = '/farmer-claim'
+
+module.exports = [{
   method: 'GET',
   path: '/farmer-claim/visit-review',
   options: {
@@ -21,7 +27,11 @@ module.exports = {
         { key: { text: 'Date of review:' }, value: { text: new Date(vetVisit.visitDate).toLocaleDateString('en-GB', { day: '2-digit', month: 'long', year: 'numeric' }) } },
         { key: { text: 'Payment amount:' }, value: { text: `£${paymentAmount}` } }
       ]
-      return h.view('farmer-claim/visit-review', { listData: { rows }, email: claimData.organisation.email })
+      return h.view('farmer-claim/visit-review', {
+        ...getYesNoRadios(legendText, radioId, claim.review),
+        listData: { rows },
+        email: claimData.organisation.email
+      })
     }
   }
-}
+}]
