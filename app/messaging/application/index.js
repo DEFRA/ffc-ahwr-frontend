0f72d2d515e1105ac1a42890c265e9bf6b0f19c9
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-05-15 17:18:20 +0100

    feat: add test for application messaging

--- app/messaging/application/index.js
+++ app/messaging/application/index.js
@@ -1,13 +1,13 @@
 const { sendMessage, receiveMessage } = require('../')
 const { applicationRequestQueue, fetchApplicationRequestMsgType, fetchClaimRequestMsgType, applicationResponseQueue } = require('../../config')
 
-function getApplication (applicationReference, sessionId) {
-  sendMessage({ applicationReference }, fetchApplicationRequestMsgType, applicationRequestQueue, { sessionId })
+async function getApplication (applicationReference, sessionId) {
+  await sendMessage({ applicationReference }, fetchApplicationRequestMsgType, applicationRequestQueue, { sessionId })
   return receiveMessage(sessionId, applicationResponseQueue)
 }
 
-function getClaim (email, sessionId) {
-  sendMessage({ email }, fetchClaimRequestMsgType, applicationRequestQueue, { sessionId })
+async function getClaim (email, sessionId) {
+  await sendMessage({ email }, fetchClaimRequestMsgType, applicationRequestQueue, { sessionId })
   return receiveMessage(sessionId, applicationResponseQueue)
 }
 
