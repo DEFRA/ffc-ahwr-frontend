b992764b2f2dc20555aee5539a95798aa8dfe013
Satish Chatap <satish.chatap@capgemini.com>
2022-05-17 11:50:24 +0100

    made test token optional

--- app/config/index.js
+++ app/config/index.js
@@ -74,7 +74,7 @@ const schema = Joi.object({
     usersFile: Joi.string().default('users.json')
   },
   useRedis: Joi.boolean().default(false),
-  testToken: Joi.string().uuid()
+  testToken: Joi.string().uuid().optional()
 })
 
 const sharedConfig = {
