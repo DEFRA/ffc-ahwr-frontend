c1223da438f383b28f7f7af9dda05ed10899be3a
Shivanshu Patel <shivanshuit914@gmail.com>
2022-05-18 14:25:01 +0100

    feat: add farmer apply review page

---
+++ app/routes/helpers/species-radio.js
@@ -0,0 +1,58 @@
+/**
+ * Generate GOV.UK radios component.
+ *
+ * @param {string} legendText heading text for radios.
+ * @param {string} id of the radio component.
+ * @param {string} previousAnswer either `yes` or `no`, used to set the radio as checked.
+ * @param {string} [errorText] error message to display.
+ * @param {object} [options] an object containing `isPageHeading` (boolean, determines if the legendText is marked as a heading), `legendClasses` (string, contains classes to add to legendText), `inline` (boolean determines if the class to make the radios display as inline is added) and hintHtml (html string to display hint message with the radio button).
+ *
+ * @return {object} object with `radios` property containing radios component.
+ */
+ function speciesRadios (legendText, id, previousAnswer, errorText = undefined, options = {}) {
+  const { isPageHeading = true, legendClasses = 'govuk-fieldset__legend--l', inline = true, hintHtml = '' } = options
+  return {
+    radios: {
+      classes: inline ? 'govuk-radios--inline' : undefined,
+      idPrefix: id,
+      name: id,
+      fieldset: {
+        legend: {
+          text: legendText,
+          isPageHeading,
+          classes: legendClasses
+        }
+      },
+      hint: {
+        html: hintHtml
+      },
+      items: [
+        {
+          value: 'beef-cattle',
+          text: 'Beef cattle',
+          checked: previousAnswer === 'beef-cattle'
+        },
+        {
+          value: 'dairy-cattle',
+          text: 'Dairy cattle',
+          checked: previousAnswer === 'dairy-cattle'
+        },
+        {
+          value: 'sheep',
+          text: 'Sheep',
+          checked: previousAnswer === 'sheep'
+        },
+        {
+          value: 'pigs',
+          text: 'Pigs',
+          checked: previousAnswer === 'pigs'
+        }
+      ],
+      ...(errorText ? { errorMessage: { text: errorText } } : {})
+    }
+  }
+}
+
+module.exports = {
+  speciesRadios
+}
