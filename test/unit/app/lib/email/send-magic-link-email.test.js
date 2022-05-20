1f3adb364fef293940fe047878e706dd97ab119d
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-05-20 12:15:51 +0100

    refactor: extend tests

--- test/unit/app/lib/email/send-magic-link-email.test.js
+++ test/unit/app/lib/email/send-magic-link-email.test.js
@@ -1,70 +1,88 @@
 const sendMagicLinkEmail = require('../../../../../app/lib/email/send-magic-link-email')
-const notifyClient = require('../../../../../app/lib/email/notify-client')
+const { notify: { templateIdFarmerApplyLogin, templateIdFarmerClaimLogin, templateIdVetLogin }, serviceUri } = require('../../../../../app/config')
+const { farmerApply, farmerClaim, vet } = require('../../../../../app/constants/user-types')
 
-notifyClient.sendEmail = jest.fn().mockResolvedValueOnce(true)
-const validEmail = 'test@unit-test.com'
-const cacheData = { }
-const config = require('../../../../../app/config')
-jest.mock('../../../../../app/config')
-const users = require('../../../../../app/api-requests/users')
-jest.mock('../../../../../app/api-requests/users')
+const getToken = require('../../../../../app/lib/get-token')
+jest.mock('../../../../../app/lib/get-token')
+
 const sendEmail = require('../../../../../app/lib/email/send-email')
 jest.mock('../../../../../app/lib/email/send-email')
-sendEmail.mockResolvedValue(true)
-const testToken = '644a2a30-7487-4e98-a908-b5ecd82d5225'
-config.testToken = testToken
-users.getByEmail.mockResolvedValue({ email: validEmail, isTest: 'yes' })
+let cacheData = { }
 const requestGetMock = {
   server:
       {
         app:
           {
             magiclinkCache: {
-              get: (_email) => {
-                return cacheData[_email]
+              get: (key) => {
+                return cacheData[key]
               },
-              set: (_email, _tokens) => {
-                cacheData[_email] = _tokens
+              set: (key, value) => {
+                cacheData[key] = value
               }
             }
           }
       }
 }
+
 describe('Send Magic Link test', () => {
-  test.each([
-    { email: validEmail, token: testToken, data: {} },
-    { email: validEmail, token: null, data: {} }
-  ])('Returns success on call sendVetMagicLinkEmail', async ({ email, data, token }) => {
+  const email = 'test@unit-test.com'
+  const sendEmailResponse = true
+  const testToken = '644a2a30-7487-4e98-a908-b5ecd82d5225'
+
+  beforeEach(() => {
+    cacheData = {}
+    jest.resetAllMocks()
+
+    sendEmail.mockResolvedValue(sendEmailResponse)
+  })
+
+  test('sends email for vet login', async () => {
+    const token = testToken
+    getToken.mockResolvedValue(token)
+    const data = {}
+
     const response = await sendMagicLinkEmail.sendVetMagicLinkEmail(requestGetMock, email, data)
-    expect(response).toBeTruthy()
-    if (token) {
-      expect(cacheData[token]).not.toBeNull()
-    } else {
-      expect(cacheData[email]).not.toBeNull()
-    }
+
+    expect(response).toEqual(sendEmailResponse)
+    expect(cacheData[email]).toEqual([token])
+    expect(cacheData[token]).toEqual({ email, redirectTo: 'vet/visit-date', userType: vet, data })
+    expect(sendEmail).toHaveBeenCalledTimes(1)
+    expect(sendEmail).toHaveBeenCalledWith(templateIdVetLogin, email, {
+      personalisation: { magiclink: `${serviceUri}/verify-login?token=${token}&email=${email}` },
+      reference: token
+    })
   })
-  test.each([
-    { email: validEmail, testToken },
-    { email: validEmail, testToken: null }
-  ])('Returns success on call sendFarmerApplyLoginMagicLink', async ({ email, token }) => {
+
+  test('sends email for farmer apply', async () => {
+    const token = testToken
+    getToken.mockResolvedValue(token)
+
     const response = await sendMagicLinkEmail.sendFarmerApplyLoginMagicLink(requestGetMock, email)
-    expect(response).toBeTruthy()
-    if (token) {
-      expect(cacheData[token]).not.toBeNull()
-    } else {
-      expect(cacheData[email]).not.toBeNull()
-    }
+
+    expect(response).toEqual(sendEmailResponse)
+    expect(cacheData[email]).toEqual([token])
+    expect(cacheData[token]).toEqual({ email, redirectTo: 'farmer-apply/org-review', userType: farmerApply })
+    expect(sendEmail).toHaveBeenCalledTimes(1)
+    expect(sendEmail).toHaveBeenCalledWith(templateIdFarmerApplyLogin, email, {
+      personalisation: { magiclink: `${serviceUri}/verify-login?token=${token}&email=${email}` },
+      reference: token
+    })
   })
-  test.each([
-    { email: validEmail, testToken },
-    { email: validEmail, testToken: null }
-  ])('Returns success on call sendFarmerClaimLoginMagicLink', async ({ email, token }) => {
+
+  test('sends email for farmer claim', async () => {
+    const token = testToken
+    getToken.mockResolvedValue(token)
+
     const response = await sendMagicLinkEmail.sendFarmerClaimLoginMagicLink(requestGetMock, email)
-    expect(response).toBeTruthy()
-    if (token) {
-      expect(cacheData[token]).not.toBeNull()
-    } else {
-      expect(cacheData[email]).not.toBeNull()
-    }
+
+    expect(response).toEqual(sendEmailResponse)
+    expect(cacheData[email]).toEqual([token])
+    expect(cacheData[token]).toEqual({ email, redirectTo: 'farmer-claim/visit-review', userType: farmerClaim })
+    expect(sendEmail).toHaveBeenCalledTimes(1)
+    expect(sendEmail).toHaveBeenCalledWith(templateIdFarmerClaimLogin, email, {
+      personalisation: { magiclink: `${serviceUri}/verify-login?token=${token}&email=${email}` },
+      reference: token
+    })
   })
 })
