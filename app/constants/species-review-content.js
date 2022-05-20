063db636865e8840bb463fbe17c3ad1b1071acf7
Shivanshu Patel <shivanshuit914@gmail.com>
2022-05-20 14:46:22 +0100

    feat: change check answer page

---
+++ app/constants/species-review-content.js
@@ -0,0 +1,18 @@
+module.exports = {
+  beef: {
+   reivewType: 'Beef cattle',
+   liveStockNumber: 'At least 11 beef cattle'
+  },
+  dairy: {
+    reivewType: 'Dairy cattle',
+    liveStockNumber: 'At least 11 dairy cattle'
+  },
+  pigs: {
+    reivewType: 'Pigs',
+    liveStockNumber: 'At least 51 pigs'
+  },
+  sheep: {
+    reivewType: 'Sheep',
+    liveStockNumber: 'At least 21 sheep'
+  }
+}
