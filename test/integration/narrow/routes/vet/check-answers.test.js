a4b9bf83e3a930bbc5186fdac11279774f7cdf75
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-05-24 12:45:38 +0100

    feat: consistently use sentence case for review answers (#70)
    * feat: consistently use sentence case on answers
    
    * chore: increment version

--- test/integration/narrow/routes/vet/check-answers.test.js
+++ test/integration/narrow/routes/vet/check-answers.test.js
@@ -29,14 +29,14 @@ describe('Vet check answers test', () => {
     })
 
     test.each([
-      { speciesTest: species.pigs, data: { whichReview: species.pigs }, elgibleNumberOfAnimals: 'no', speciesTestText: 'PRRS in herd', speciesTestValue: { pigsTest: 'no' }, speciesTestResultValue: 'No', reviewReport: 'no' },
-      { speciesTest: species.sheep, data: { whichReview: species.sheep }, elgibleNumberOfAnimals: 'no', speciesTestText: 'Percentage reduction in eggs per gram (EPG)', speciesTestValue: { sheepTest: 100 }, speciesTestResultValue: 100, reviewReport: 'no' },
-      { speciesTest: species.beef, data: { whichReview: species.beef }, elgibleNumberOfAnimals: 'no', speciesTestText: 'BVD in herd', speciesTestValue: { beefTest: 'no' }, speciesTestResultValue: 'No', reviewReport: 'no' },
-      { speciesTest: species.dairy, data: { whichReview: species.dairy }, elgibleNumberOfAnimals: 'no', speciesTestText: 'BVD in herd', speciesTestValue: { dairyTest: 'no' }, speciesTestResultValue: 'No', reviewReport: 'no' },
-      { speciesTest: species.pigs, data: { whichReview: species.pigs }, elgibleNumberOfAnimals: 'yes', speciesTestText: 'PRRS in herd', speciesTestValue: { pigsTest: 'yes' }, speciesTestResultValue: 'Yes', reviewReport: 'yes' },
-      { speciesTest: species.sheep, data: { whichReview: species.sheep }, elgibleNumberOfAnimals: 'yes', speciesTestText: 'Percentage reduction in eggs per gram (EPG)', speciesTestValue: { sheepTest: 0 }, speciesTestResultValue: 0, reviewReport: 'yes' },
-      { speciesTest: species.beef, data: { whichReview: species.beef }, elgibleNumberOfAnimals: 'yes', speciesTestText: 'BVD in herd', speciesTestValue: { beefTest: 'yes' }, speciesTestResultValue: 'Yes', reviewReport: 'yes' },
-      { speciesTest: species.dairy, data: { whichReview: species.dairy }, elgibleNumberOfAnimals: 'yes', speciesTestText: 'BVD in herd', speciesTestValue: { dairyTest: 'yes' }, speciesTestResultValue: 'Yes', reviewReport: 'yes' }
+      { speciesTest: species.pigs, data: { whichReview: species.pigs }, elgibleNumberOfAnimals: 'No', speciesTestText: 'PRRS in herd', speciesTestValue: { pigsTest: 'no' }, speciesTestResultValue: 'No', reviewReport: 'No' },
+      { speciesTest: species.sheep, data: { whichReview: species.sheep }, elgibleNumberOfAnimals: 'No', speciesTestText: 'Percentage reduction in eggs per gram (EPG)', speciesTestValue: { sheepTest: 100 }, speciesTestResultValue: 100, reviewReport: 'No' },
+      { speciesTest: species.beef, data: { whichReview: species.beef }, elgibleNumberOfAnimals: 'No', speciesTestText: 'BVD in herd', speciesTestValue: { beefTest: 'no' }, speciesTestResultValue: 'No', reviewReport: 'No' },
+      { speciesTest: species.dairy, data: { whichReview: species.dairy }, elgibleNumberOfAnimals: 'No', speciesTestText: 'BVD in herd', speciesTestValue: { dairyTest: 'no' }, speciesTestResultValue: 'No', reviewReport: 'No' },
+      { speciesTest: species.pigs, data: { whichReview: species.pigs }, elgibleNumberOfAnimals: 'Yes', speciesTestText: 'PRRS in herd', speciesTestValue: { pigsTest: 'yes' }, speciesTestResultValue: 'Yes', reviewReport: 'Yes' },
+      { speciesTest: species.sheep, data: { whichReview: species.sheep }, elgibleNumberOfAnimals: 'Yes', speciesTestText: 'Percentage reduction in eggs per gram (EPG)', speciesTestValue: { sheepTest: 0 }, speciesTestResultValue: 0, reviewReport: 'Yes' },
+      { speciesTest: species.beef, data: { whichReview: species.beef }, elgibleNumberOfAnimals: 'Yes', speciesTestText: 'BVD in herd', speciesTestValue: { beefTest: 'yes' }, speciesTestResultValue: 'Yes', reviewReport: 'Yes' },
+      { speciesTest: species.dairy, data: { whichReview: species.dairy }, elgibleNumberOfAnimals: 'Yes', speciesTestText: 'BVD in herd', speciesTestValue: { dairyTest: 'yes' }, speciesTestResultValue: 'Yes', reviewReport: 'Yes' }
     ])('returns 200 with answers for specific claim type - $species', async ({ data, elgibleNumberOfAnimals, speciesTestText, speciesTestValue, speciesTestResultValue, reviewReport }) => {
       const visitDate = new Date(2022, 4, 12)
       session.getVetVisitData.mockReturnValueOnce({
