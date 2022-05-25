06cfeaf1a40328ec79ff5983384182bf00d41c23
Shivanshu Patel <shivanshuit914@gmail.com>
2022-05-25 10:43:23 +0100

    fix: typo for pigs

--- app/constants/species-content-vet.js
+++ app/constants/species-content-vet.js
@@ -11,8 +11,8 @@ module.exports = {
   },
   pigs: {
     errorText: 'Select yes if there were more than 51 pigs on the farm at the time of the review',
-    legendText: 'Were there more than 51 pig on the farm at the time of the review?',
-    title: 'Were there more than 51 pig on the farm at the time of the review?'
+    legendText: 'Were there more than 51 pigs on the farm at the time of the review?',
+    title: 'Were there more than 51 pigs on the farm at the time of the review?'
   },
   sheep: {
     errorText: 'Select yes if there were 21 or more sheep on the farm at the time of the review',
