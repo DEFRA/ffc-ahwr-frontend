0cea77f5869b58dc94e55428b8a282d55f14bec1
CORP\venkgann <venkata.gannavarapu@capgemini.com>
2022-06-20 12:27:39 +0100

    final commit

--- test/acceptance/features/farmer-apply/farmer-apply.feature
+++ test/acceptance/features/farmer-apply/farmer-apply.feature
@@ -3,14 +3,19 @@ Feature: Farmer apply
   Scenario: Farmer receives email when signing in
     Given I open the site "/farmer-apply/login"
     Then I expect that the title contains "Sign in - Review the health and welfare of your livestock"
+    #Then I expect that the title contains "Review the health and welfare of your livestock - GOV.UK"
     And I expect that element "h1" contains the text "Enter your email address"
     When I click on the button "#submit"
 
   @wip
   Scenario: Get Login Token
     Given I open the site "/farmer-apply/login"
-    When I enter my valid "email"
-    And I click on button
+    When I enter my valid "venkata.gannavarapu@capgemini.com"
+    
+
+    
+    Given I open the site "/verify-login?token=0f9ecf32-da18-4e17-8a94-c37732d97489&email=venkata.gannavarapu%40capgemini.com"
+    Then I expect that the title contains "Check your details"
 
     
     
--- test/acceptance/page-objects/farmer-org-review.js
+++ test/acceptance/page-objects/farmer-org-review.js
@@ -8,25 +8,30 @@ class FarmerOrgReview extends Page {
 //Address:	Address line 1, Addres line 2, Town, AB12 34CD
 //Contact email address:	venkata.gannavarapu@capgemini.com
 //If your email is not correct, update your email address at Rural Payments
-  get header () { return $('#main-content h1') }
-  get orgName () { return $('#main-content h2') }
-  get start () { return $('#main-content a') }
+  get continue () { return $('##btnContinue') }
+  get orgReviewQuestion () { return $('.govuk-form-group legend') }
+  get orgYesRadioOption () { return $('.govuk-form-group #confirmCheckDetails') }
+  get orgNoRadioOption () { return $('.govuk-form-group #confirmCheckDetails-2') }
 
   open () {
     super.open('')
     browser.pause(3000)
   }
 
-  async getHeader () {
-    await this.header.getText()
+  async getOrgReviewQuestion () {
+    await this.orgReviewQuestion.getText()
   }
 
-  async getOrgName () {
-    await this.orgName.getText()
+  async selectYes () {
+    await this.orgYesRadioOption.click()
   }
 
-  async clickStartSignin () {
-    await this.start.click()
+  async selectNo () {
+    await this.orgNoRadioOption.click()
+  }
+
+  async clickContinue () {
+    await this.continue.click()
   }
 
 }
