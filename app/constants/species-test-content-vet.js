19d72516bebb9e19ebdc992c66a6fa2eb8fb4f41
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-07-02 12:58:48 +0100

    feat: update species content for current questions (#101)
    * chore: increment version
    
    * feat: update species content for current questions

--- app/constants/species-test-content-vet.js
+++ app/constants/species-test-content-vet.js
@@ -1,17 +1,17 @@
 module.exports = {
   beef: {
-    errorText: 'Select yes if BVD was found in the herd',
+    errorText: 'Select yes if test results showed BVD in the herd',
     legendText: 'Did antibody test results show that BVD is in the herd?',
-    title: 'Vet BVD result'
+    title: 'Did antibody test results show that BVD is in the herd?'
   },
   dairy: {
-    errorText: 'Select yes if BVD was found in the herd',
+    errorText: 'Select yes if test results showed BVD in the herd',
     legendText: 'Did testing results show that BVD is in the herd?',
-    title: 'Milk Test BVD result'
+    title: 'Did testing results show that BVD is in the herd?'
   },
   pigs: {
-    errorText: 'Select yes if PRRS was found in the herd',
-    legendText: 'Did test results show that PRRS in the herd?',
-    title: 'Vet PRRS result'
+    errorText: 'Select yes if test results showed PRRS in the herd',
+    legendText: 'Did test results show that PRRS is in the herd?',
+    title: 'Did test results show that PRRS is in the herd?'
   }
 }
--- test/integration/narrow/routes/farmer-apply/which-review.test.js
+++ test/integration/narrow/routes/farmer-apply/which-review.test.js
@@ -82,7 +82,7 @@ describe('Species review test', () => {
       const res = await global.__SERVER__.inject(options)
 
       const $ = cheerio.load(res.payload)
-      expect($('p.govuk-error-message').text()).toMatch('Select which livestock do you want a review for?')
+      expect($('p.govuk-error-message').text()).toMatch('Select the livestock you would like reviewed')
       expect(res.statusCode).toBe(400)
     })
 
