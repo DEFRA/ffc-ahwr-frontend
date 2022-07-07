545eea2548d48ef6fc4a0247e03d8fd2a09cc585
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-07-07 13:27:18 +0100

    chore: content update

--- app/constants/species-test-content-vet.js
+++ app/constants/species-test-content-vet.js
@@ -10,8 +10,8 @@ module.exports = {
     title: 'Did testing results show that BVD is in the herd?'
   },
   pigs: {
-    errorText: 'Select yes if test results showed PRRS in the herd',
-    legendText: 'Did test results show that PRRS is in the herd?',
-    title: 'Did test results show that PRRS is in the herd?'
+    errorText: 'Select yes if test results showed PRRS was in the herd',
+    legendText: 'Did test results show that PRRS was in the herd?',
+    title: 'Did test results show that PRRS was in the herd?'
   }
 }
